14c621130826c6d0e4cdf5f4f361aa70450bb813
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-12-09 18:39:43 -0500

    ran som sql queries and added server

--- queries.sql
+++ queries.sql
@@ -1,12 +1,63 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+    SELECT * FROM [Customers] where PostalCode ='1010';
+Number of Records: 3
+CustomerID	CustomerName	ContactName	Address	City	PostalCode	Country
+12	Cactus Comidas para llevar	Patricio Simpson	Cerrito 333	Buenos Aires	1010	Argentina
+54	Océano Atlántico Ltda.	Yvonne Moncada	Ing. Gustavo Moncada 8585 Piso 20-A	Buenos Aires	1010	Argentina
+64	Rancho grande	Sergio Gutiérrez	Av. del Libertador 900	Buenos Aires	1010	Argentina
+
+
 
 -- Find the phone number for the supplier with the id 11
-
+    SELECT Phone FROM [Suppliers] where SupplierID='11';
+    Number of Records: 1
+Phone
+(010) 9984510
 -- List first 10 orders placed, sorted descending by the order date
-
+Number of Records: 10
+OrderDate
+1996-07-04
+1996-07-05
+1996-07-08
+1996-07-08
+1996-07-09
+1996-07-10
+1996-07-11
+1996-07-12
+1996-07-15
+1996-07-16
 -- Find all customers that live in London, Madrid, or Brazil
+    SELECT * FROM [Customers] where city='London';
+Number of Records: 6
+CustomerID	CustomerName	ContactName	Address	City	PostalCode	Country
+4	Around the Horn	Thomas Hardy	120 Hanover Sq.	London	WA1 1DP	UK
+11	B's Beverages	Victoria Ashworth	Fauntleroy Circus	London	EC2 5NT	UK
+16	Consolidated Holdings	Elizabeth Brown	Berkeley Gardens 12 Brewery	London	WX1 6LT	UK
+19	Eastern Connection	Ann Devon	35 King George	London	WX3 6FW	UK
+53	North/South	Simon Crowther	South House 300 Queensbridge	London	SW7 1RZ	UK
+72	Seven Seas Imports	Hari Kumar	90 Wadhurst Rd.	London	OX15 4NB	UK;
+
+SELECT * FROM [Customers] where city='Madrid';
+Number of Records: 3
+CustomerID	CustomerName	ContactName	Address	City	PostalCode	Country
+8	Bólido Comidas preparadas	Martín Sommer	C/ Araquil, 67	Madrid	28023	Spain
+22	FISSA Fabrica Inter. Salchichas S.A.	Diego Roel	C/ Moralzarzal, 86	Madrid	28034	Spain
+69	Romero y tomillo	Alejandra Camino	Gran Vía, 1	Madrid	28001	Spain
+
+SELECT * FROM [Customers] where country='Brazil';
+Number of Records: 9
+CustomerID	CustomerName	ContactName	Address	City	PostalCode	Country
+15	Comércio Mineiro	Pedro Afonso	Av. dos Lusíadas, 23	São Paulo	05432-043	Brazil
+21	Familia Arquibaldo	Aria Cruz	Rua Orós, 92	São Paulo	05442-030	Brazil
+31	Gourmet Lanchonetes	André Fonseca	Av. Brasil, 442	Campinas	04876-786	Brazil
+34	Hanari Carnes	Mario Pontes	Rua do Paço, 67	Rio de Janeiro	05454-876	Brazil
+61	Que Delícia	Bernardo Batista	Rua da Panificadora, 12	Rio de Janeiro	02389-673	Brazil
+62	Queen Cozinha	Lúcia Carvalho	Alameda dos Canàrios, 891	São Paulo	05487-020	Brazil
+67	Ricardo Adocicados	Janete Limeira	Av. Copacabana, 267	Rio de Janeiro	02389-890	Brazil
+81	Tradição Hipermercados	Anabela Domingues	Av. Inês de Castro, 414	São Paulo	05634-030	Brazil
+88	Wellington Importadora	Paula Parente	Rua do Mercado, 12	Resende	08737-363	Brazil
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
